4ad3f19b49177e860f22b6143a2a464f03ad3d9c
chrissou <nicolchristelle@gmail.com>
2018-01-27 16:31:56 +0100

    ajout-skill

--- site perso/js/app.js
+++ site perso/js/app.js
@@ -3,14 +3,14 @@ var app = {
   init: function() {
     console.log('App : init');
 
-}
+$('.title').hover(function (){
+    $( this ).fadeOut( 2000 );
+  $( this ).fadeIn( 2000 );
+
+});
 
 
-
-
-
-
-
+},
 
 };
 // quand le DOM est prêt, on lance app.init
